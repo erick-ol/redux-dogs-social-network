e6cbb9932863f914392d616fa56aa1f02ca1b42f
Erick Santos <erickosantos.dev@gmail.com>
2022-04-23 21:25:31 -0300

    feat: 🎸 creating photoPost reducer

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -6,13 +6,14 @@ import {
 
 // reducers
 import photo from './photo';
+import photoPost from './photoPost';
 import token from './token';
 import user from './user';
 import feed from './feed';
 import ui from './ui';
 
 const middleware = [...getDefaultMiddleware()];
-const reducer = combineReducers({ photo, token, user, feed, ui });
+const reducer = combineReducers({ photo, token, user, feed, ui, photoPost });
 const store = configureStore({ reducer, middleware });
 
 export default store;
